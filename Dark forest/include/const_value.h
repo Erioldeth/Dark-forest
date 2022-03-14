f261c4d61f2e80e52ddfe448b4393458bd90a05a
Zarikhas <94889158+Zarikhas@users.noreply.github.com>
2022-03-14 21:09:17 +0700

    Add files via upload

---
+++ Dark forest/include/const_value.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef CONST
+#define CONST
+const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 640;
+
+const int ACCELERATOR = 1;
+
+const int MAX_SKULL = 5;
+const int MAX_REAPER = 3;
+
+const int BUTTON_WIDTH = 100;
+const int BUTTON_HEIGHT = 70;
+
+const int MIN_VOLUME = 0;
+const int MAX_VOLUME = 100;
+#endif // !CONST
