c3e4a82250616ad17b23f3ae31bb7a920aa994a5
Zarikhas <94889158+Zarikhas@users.noreply.github.com>
2022-03-17 17:37:51 +0700

    Add files via upload

---
+++ dark forest/include/close.h
@@ -0,0 +1,83 @@
+//#pragma once
+#ifndef CLOSE
+#define CLOSE
+#include<fstream>
+#include"basicVariable.h"
+#include"advancedVariable.h"
+void close() {
+	//close shared items
+	menu_background.free();
+	transition_screen.free();
+	back.free();
+	TTF_CloseFont(button_font);
+	TTF_CloseFont(deco_font);
+	button_font = deco_font = nullptr;
+	Mix_FreeChunk(touch_button);
+	Mix_FreeChunk(click_button);
+	touch_button = click_button = nullptr;
+
+	//close menu
+	menu_title.free();
+	play.free();
+	setting.free();
+	high_score.free();
+	Mix_FreeMusic(menu_music);
+	menu_music = nullptr;
+
+	//close game play
+	game_background.free();
+	game_map.free();
+	support_board.free();
+	dead_background.free();
+	platform.clear();
+
+	player.free();
+
+	skull_warning.free();
+	for(auto& e : skull) e.free();
+	skull.clear();
+	skull_curse.clear();
+	exploded_texture.free();
+	Mix_FreeChunk(exploded_sound);
+	exploded_sound = nullptr;
+
+	reaper_warning.free();
+	while(!reaper_death.empty()) {
+		get<0>(reaper_death[0]).free();
+		reaper_death.erase(begin(reaper_death));
+	}
+	for(auto& e : reaper) e.free();
+	reaper.clear();
+	reaper_curse.clear();
+	shadow_caught_texture.free();
+	Mix_FreeChunk(shadow_caught_sound);
+	shadow_caught_sound = nullptr;
+
+	easy_mode.free();
+	normal_mode.free();
+	hard_mode.free();
+	pause_game.free();
+	resume_game.free();
+	exit_game.free();
+
+	//close setting
+
+	//close high score
+	std::ofstream new_data("high_score_data.txt");
+	for(auto& sd : score_data) if(!get<0>(sd).empty()) new_data << get<0>(sd) << "\n" << get<1>(sd) << ' ' << get<2>(sd) << "\n";
+	score_data.clear();
+	resetScore.free();
+
+	//close renderer and window
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	renderer = nullptr;
+	window = nullptr;
+
+	//close all tool
+	IMG_Quit();
+	TTF_Quit();
+	Mix_Quit();
+	SDL_Quit();
+}
+#endif // !CLOSE
---
+++ dark forest/include/particle.h
@@ -0,0 +1,50 @@
+//#pragma once
+#ifndef PARTICLE
+#define PARTICLE
+#include"randomMachine.h"
+#include"texture.h"
+class Particle {
+	Texture particle_texture;
+	std::vector<std::tuple<SDL_Point, SDL_Rect, int>> particle_info;
+public:
+	Particle() {
+		particle_info.resize(MAX_PARTICLE);
+	}
+	void loadParticleTexture(std::string path, Uint8 r = 1, Uint8 g = 1, Uint8 b = 1) {
+		particle_texture.loadFromFile(path, r, g, b);
+		particle_texture.setBlendMode(SDL_BLENDMODE_BLEND);
+	}
+	void setParticleSize(int w, int h) {
+		particle_texture.setSize(w, h);
+	}
+	void setParticleColor(Uint8 r, Uint8 g, Uint8 b) {
+		particle_texture.setColor(r, g, b);
+	}
+	void render(SDL_Rect& region, int totalSprite, int col, int original_width, int original_height) {
+		for(auto& p : particle_info) {
+			if(get<2>(p) == 0) {
+				std::uniform_int_distribution<> horizontal_change(-5, region.w + 5 - particle_texture.textureWidth());
+				std::uniform_int_distribution<> vertical_change(-5, region.h + 5 - particle_texture.textureHeight());
+				get<0>(p) = {region.x + horizontal_change(mt),region.y + vertical_change(mt)};
+
+				std::uniform_int_distribution<> rand_sprite(0, totalSprite - 1);
+				int frame = rand_sprite(mt);
+				get<1>(p) = {original_width * (frame % col),original_height * (frame / col),original_width,original_height};
+
+				get<2>(p) = particle_life_init(mt);
+			}
+			short a = get<2>(p) * 255 / MAX_PARTICLE_LIFE;
+			particle_texture.setAlpha((Uint8)a);
+			particle_texture.render(get<0>(p).x, get<0>(p).y, &get<1>(p));
+			get<2>(p)--;
+		}
+	}
+	void reset() {
+		for(auto& p : particle_info) get<2>(p) = 0;
+	}
+	void free() {
+		particle_texture.free();
+		particle_info.clear();
+	}
+};
+#endif // !PARTICLE
